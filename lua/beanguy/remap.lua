8d2cc54cea8c7c3c3d46ad748cf024ddc231530d
rjw <robbiejohnwallace@yahoo.co.uk>
2025-12-19 14:27:34 +0000

    better tab nagivation.

--- lua/beanguy/remap.lua
+++ lua/beanguy/remap.lua
@@ -1,1 +1,7 @@
 vim.g.mapleader = " "
+
+-- Tabs & Terminals
+vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h", {silent = true})
+vim.keymap.set("n", "<leader>tbn", vim.cmd.tabNext, {silent = true})
+vim.keymap.set("n", "<leader>tbp", vim.cmd.tabprevious, {silent = true})
+vim.keymap.set("n", "<leader>tbc", vim.cmd.tabnew, {silent = true})
