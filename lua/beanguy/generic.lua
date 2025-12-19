ec643539d1f3f24cc8098d4d49a294a51ef43995
rjw <robbiejohnwallace@yahoo.co.uk>
2025-12-19 10:46:28 +0000

    Whitespace & nvim-tree enhancements.

--- lua/beanguy/generic.lua
+++ lua/beanguy/generic.lua
@@ -1,3 +1,7 @@
+-- Numbers
 vim.cmd("set rnu nu")
-vim.cmd("set ma")
 vim.cmd("set shiftwidth=4 smarttab")
+
+-- Whitespace
+vim.o.list = true
+vim.o.listchars = 'tab:» ,lead:·,trail:·,leadmultispace:· '
